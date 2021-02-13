843974926fdaddb7b28f1ae0362bf011f6f352fd
Adam Fowler <adamfowler71@gmail.com>
2021-02-13 16:48:23 +0000

    Add upgrade handlers

--- Sources/HummingbirdCore/Server/ChannelInitializer.swift
+++ Sources/HummingbirdCore/Server/ChannelInitializer.swift
@@ -1,4 +1,5 @@
 import NIO
+import NIOHTTP1
 
 /// HTTPServer child channel initializer protocol
 public protocol HBChannelInitializer {
@@ -7,14 +8,23 @@ public protocol HBChannelInitializer {
 
 /// Setup child channel for HTTP1
 public struct HTTP1ChannelInitializer: HBChannelInitializer {
-    public init() {}
+    public init(upgraders: [HTTPServerProtocolUpgrader] = []) {
+        self.upgraders = upgraders
+    }
 
     public func initialize(channel: Channel, childHandlers: [ChannelHandler], configuration: HBHTTPServer.Configuration) -> EventLoopFuture<Void> {
+        var serverUpgrade: NIOHTTPServerUpgradeConfiguration?
+        if self.upgraders.count > 0 {
+            serverUpgrade = (self.upgraders, { _ in })
+        }
         return channel.pipeline.configureHTTPServerPipeline(
             withPipeliningAssistance: configuration.withPipeliningAssistance,
+            withServerUpgrade: serverUpgrade,
             withErrorHandling: true
         ).flatMap {
             return channel.pipeline.addHandlers(childHandlers)
         }
     }
+
+    let upgraders: [HTTPServerProtocolUpgrader]
 }
