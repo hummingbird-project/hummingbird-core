d3c0281da5a6108580c749c82eeaa6e37550b306
Adam Fowler <adamfowler71@gmail.com>
2021-02-13 15:30:52 +0000

    ChannelInitializer takes an array of ChannelHandlers

--- Sources/HummingbirdCore/Server/ChannelInitializer.swift
+++ Sources/HummingbirdCore/Server/ChannelInitializer.swift
@@ -2,19 +2,19 @@ import NIO
 
 /// HTTPServer child channel initializer protocol
 public protocol HBChannelInitializer {
-    func initialize(_ server: HBHTTPServer, channel: Channel, responder: HBHTTPResponder) -> EventLoopFuture<Void>
+    func initialize(channel: Channel, childHandlers: [ChannelHandler], configuration: HBHTTPServer.Configuration) -> EventLoopFuture<Void>
 }
 
 /// Setup child channel for HTTP1
 public struct HTTP1ChannelInitializer: HBChannelInitializer {
     public init() {}
 
-    public func initialize(_ server: HBHTTPServer, channel: Channel, responder: HBHTTPResponder) -> EventLoopFuture<Void> {
+    public func initialize(channel: Channel, childHandlers: [ChannelHandler], configuration: HBHTTPServer.Configuration) -> EventLoopFuture<Void> {
         return channel.pipeline.configureHTTPServerPipeline(
-            withPipeliningAssistance: server.configuration.withPipeliningAssistance,
+            withPipeliningAssistance: configuration.withPipeliningAssistance,
             withErrorHandling: true
         ).flatMap {
-            return server.addChildHandlers(channel: channel, responder: responder)
+            return channel.pipeline.addHandlers(childHandlers)
         }
     }
 }
