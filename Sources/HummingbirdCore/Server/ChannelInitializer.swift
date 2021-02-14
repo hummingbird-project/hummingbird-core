4b5535ed3904f13b3f0a7f77a33018cce9aaf993
Adam Fowler <adamfowler71@gmail.com>
2021-02-14 20:55:10 +0000

    comment

--- Sources/HummingbirdCore/Server/ChannelInitializer.swift
+++ Sources/HummingbirdCore/Server/ChannelInitializer.swift
@@ -3,6 +3,11 @@ import NIOHTTP1
 
 /// HTTPServer child channel initializer protocol
 public protocol HBChannelInitializer {
+    /// Initialize channel
+    /// - Parameters:
+    ///   - channel: channel
+    ///   - childHandlers: Channel handlers to add
+    ///   - configuration: server configuration
     func initialize(channel: Channel, childHandlers: [RemovableChannelHandler], configuration: HBHTTPServer.Configuration) -> EventLoopFuture<Void>
 }
 
@@ -11,11 +16,17 @@ public struct HTTP1ChannelInitializer: HBChannelInitializer {
     public init(upgraders: [HTTPServerProtocolUpgrader] = []) {
         self.upgraders = upgraders
     }
-
+    
+    /// Initialize HTTP1 channel
+    /// - Parameters:
+    ///   - channel: channel
+    ///   - childHandlers: Channel handlers to add
+    ///   - configuration: server configuration
     public func initialize(channel: Channel, childHandlers: [RemovableChannelHandler], configuration: HBHTTPServer.Configuration) -> EventLoopFuture<Void> {
         var serverUpgrade: NIOHTTPServerUpgradeConfiguration?
         if self.upgraders.count > 0 {
             serverUpgrade = (self.upgraders, { channel in
+                // remove HTTP handlers after upgrade
                 childHandlers.forEach {
                     _ = channel.pipeline.removeHandler($0)
                 }
