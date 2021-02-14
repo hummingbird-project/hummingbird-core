cda01d4d299e03a6621ae07634bd84ff3964738a
Adam Fowler <adamfowler71@gmail.com>
2021-02-14 18:32:25 +0000

    Require all channel handlers to be removable

--- Sources/HummingbirdCore/Server/ChannelInitializer.swift
+++ Sources/HummingbirdCore/Server/ChannelInitializer.swift
@@ -3,19 +3,25 @@ import NIOHTTP1
 
 /// HTTPServer child channel initializer protocol
 public protocol HBChannelInitializer {
-    func initialize(channel: Channel, childHandlers: [ChannelHandler], configuration: HBHTTPServer.Configuration) -> EventLoopFuture<Void>
+    func initialize(channel: Channel, childHandlers: [RemovableChannelHandler], configuration: HBHTTPServer.Configuration) -> EventLoopFuture<Void>
 }
 
 /// Setup child channel for HTTP1
 public struct HTTP1ChannelInitializer: HBChannelInitializer {
     public init(upgraders: [HTTPServerProtocolUpgrader] = []) {
+        print("Upgraders: \(upgraders)")
         self.upgraders = upgraders
     }
 
-    public func initialize(channel: Channel, childHandlers: [ChannelHandler], configuration: HBHTTPServer.Configuration) -> EventLoopFuture<Void> {
+    public func initialize(channel: Channel, childHandlers: [RemovableChannelHandler], configuration: HBHTTPServer.Configuration) -> EventLoopFuture<Void> {
         var serverUpgrade: NIOHTTPServerUpgradeConfiguration?
         if self.upgraders.count > 0 {
-            serverUpgrade = (self.upgraders, { _ in })
+            serverUpgrade = (self.upgraders, { channel in
+                print("Core: Upgraded")
+                childHandlers.forEach {
+                    channel.pipeline.removeHandler($0)
+                }
+            })
         }
         return channel.pipeline.configureHTTPServerPipeline(
             withPipeliningAssistance: configuration.withPipeliningAssistance,
