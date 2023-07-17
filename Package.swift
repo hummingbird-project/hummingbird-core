92f6e1dc2e502cf53aea7477a35b3a460d50f9ab
Adam Fowler <adamfowler71@gmail.com>
2023-07-17 08:29:57 +0100

    Remove unused type

--- Package.swift
+++ Package.swift
@@ -66,7 +66,7 @@ let package = Package(
     ]
 )
 
-if ProcessInfo.processInfo.environment["HBCORE-STRICT_CONCURRENCY"] == "true" {
+if ProcessInfo.processInfo.environment["HBCORE_STRICT_CONCURRENCY"] == "true" {
     for target in package.targets {
         if !target.isTest {
             target.swiftSettings = [.unsafeFlags(["-Xfrontend", "-warn-concurrency"])]
